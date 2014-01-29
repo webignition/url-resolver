a6812f71bbd825e942785478145ce962249997a0
Jon Cram <jon@webignition.net>
2014-01-29 17:07:48 +0000

    Test with combination of bad response, 301 and meta redirects

--- tests/webignition/Tests/Url/Resolver/Resolve/ResolveTest.php
+++ tests/webignition/Tests/Url/Resolver/Resolve/ResolveTest.php
@@ -8,9 +8,8 @@ use webignition\Url\Resolver\Resolver;
 class ResolveTest extends BaseTest {
     
     const SOURCE_URL = 'http://example.com/';
-    const EFFECTIVE_URL = 'http://www.example.com/';    
-    
-    // test with 404 (retry without url encoding) that gets 301
+    const EFFECTIVE_URL = 'http://www.example.com/';
+
     
     public function testNoHttpRedirect() {
         $this->setHttpFixtures($this->buildHttpFixtureSet(array(
@@ -21,6 +20,43 @@ class ResolveTest extends BaseTest {
         $resolver->getConfiguration()->setBaseRequest($this->getHttpClient()->get());
         
         $this->assertEquals(self::SOURCE_URL, $resolver->resolve(self::SOURCE_URL));
+    }
+    
+    
+    public function testBadResponseWithRetryWithoutUrlEncodingTo301To200WithMetaRedirectToTarget() {
+        $this->setHttpFixtures($this->buildHttpFixtureSet(array(
+            "HTTP/1.0 404",
+            "HTTP/1.0 301\nLocation: http://example.com/foo.html",
+            "HTTP/1.0 200 OK\nContent-Type:text/html\n\n<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url=".self::EFFECTIVE_URL."\"></head></html>",
+            "HTTP/1.0 200 OK",
+        )));
+
+        $resolver = new Resolver();        
+        $resolver->getConfiguration()->setBaseRequest($this->getHttpClient()->get());
+        $resolver->getConfiguration()->enableFollowMetaRedirects();
+        $resolver->getConfiguration()->enableRetryWithUrlEncodingDisabled();
+        
+        $this->assertEquals(self::EFFECTIVE_URL, $resolver->resolve(self::SOURCE_URL));        
+        $this->assertEquals(200, $resolver->getLastResponse()->getStatusCode());
+    }
+    
+    
+    public function testBadResponseWithRetryWithoutUrlEncodingTo301To200WithMetaRedirectTo301To200WithTarget() {
+        $this->setHttpFixtures($this->buildHttpFixtureSet(array(
+            "HTTP/1.0 404",
+            "HTTP/1.0 301\nLocation: http://example.com/foo.html",
+            "HTTP/1.0 200 OK\nContent-Type:text/html\n\n<!DOCTYPE html><html><head><meta http-equiv=\"refresh\" content=\"0; url=http://example.com/bar.html\"></head></html>",
+            "HTTP/1.0 301\nLocation: " . self::EFFECTIVE_URL,
+            "HTTP/1.0 200 OK",
+        )));
+
+        $resolver = new Resolver();        
+        $resolver->getConfiguration()->setBaseRequest($this->getHttpClient()->get());
+        $resolver->getConfiguration()->enableFollowMetaRedirects();
+        $resolver->getConfiguration()->enableRetryWithUrlEncodingDisabled();
+        
+        $this->assertEquals(self::EFFECTIVE_URL, $resolver->resolve(self::SOURCE_URL));        
+        $this->assertEquals(200, $resolver->getLastResponse()->getStatusCode());        
     }
     
 }
